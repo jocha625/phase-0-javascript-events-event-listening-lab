281819285c3730afa72d77e7351b21f95c78cdba
Jorge Chavez Meza <chavezmezajorge0@gmail.com>
2023-03-07 13:04:55 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-<<<<<<< HEAD
 const input = document.getElementById('button');
-=======
-function addingEventListener() {
-    const input = document.getElementById('button');
->>>>>>> bcf02dc (complete lab)
 
 function clickAlert() {
   alert('I was clicked!');
 }
 
 input.addEventListener('click', clickAlert);
-<<<<<<< HEAD
-=======
-}
-
-
-
-
-
-
-
->>>>>>> bcf02dc (complete lab)
